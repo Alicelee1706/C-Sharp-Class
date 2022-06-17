11458c949ca4feeec9c1e5e93f340ee96d983b19
Alicelee1706 <78105672+Alicelee1706@users.noreply.github.com>
2022-06-17 23:54:45 +0700

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld
+{
+  static char[,] board;
+  static int n;
+  static char empty = '-';
+
+  static void Main ()
+  {
+    Console. Write("Bạn muốn chơi bao nhiêu ô: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("So you want to play {0}x{0} board!", n, n);
+    init ();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+	    {
+	        if (j == n - 1)
+	        {
+		        Console.Write (" {0} ", board[i, j] == empty ? ' ' : board[i, j]);
+	        } 
+	        else
+	        {
+		        Console.Write (" {0} |", board[i, j] == empty ? ' ' : board[i, j]);
+	        }
+	    }
+	    Console.WriteLine ();
+	    for (int j = 0; j < n; j++)
+	    {
+	        if (j == n - 1)
+	        {
+		        Console.Write ("---");
+	        }
+	        else
+	        {
+		    Console.Write ("---|");
+	        }
+	    }
+	    Console.WriteLine ();
+    }
+ }
+
+  private static void init ()
+  {
+    board = new char[n, n];
+    for (int i = 0; i < n; i++)
+      {
+	for (int j = 0; j < n; j++)
+	  {
+	    board[i, j] = empty;
+	  }
+      }
+
+  }
+}
